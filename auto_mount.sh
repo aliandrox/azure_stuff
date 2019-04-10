4e77ed6e058a6140600ea1491198d270089e33a0
aliandrox <1607903+aliandrox@users.noreply.github.com>
2019-04-10 23:47:42 +1200

    Update auto_mount.sh

--- auto_mount.sh
+++ auto_mount.sh
@@ -1,7 +1,3 @@
-sudo mkdir -p /data/boomi && sudo mkdir /data/files && sudo mkdir /data/export && sudo chmod 777 /data && sudo chmod 777 /data/boomi && sudo chmod 777 /data/files && sudo chmod 777 /data/export
-
-
-
 #!/bin/bash
 # $1 = Azure storage account name
 # $2 = Azure storage account key
@@ -9,5 +5,11 @@ sudo mkdir -p /data/boomi && sudo mkdir /data/files && sudo mkdir /data/export &
 # $4 = mountpoint path
 # $5 - username
 
-sudo mkdir $4
+if [ -d "/data" ] 
+then
+    sudo mount -t cifs //$1.file.core.windows.net/$3 $4 -o vers=3.0,username=$1,password=$2,dir_mode=0777,file_mode=0777,sec=ntlmssp
+else
+    sudo mkdir -p /data/boomi && sudo mkdir /data/files && sudo mkdir /data/export && sudo chmod 777 /data && sudo chmod 777 /data/boomi && sudo chmod 777 /data/files && sudo chmod 777 /data/export
+fi
+
 sudo mount -t cifs //$1.file.core.windows.net/$3 $4 -o vers=3.0,username=$1,password=$2,dir_mode=0777,file_mode=0777,sec=ntlmssp
