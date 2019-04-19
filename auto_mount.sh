5b16401a342340cdf32e16dd20eb0b5f1ad9b1f8
aliandrox <1607903+aliandrox@users.noreply.github.com>
2019-04-19 22:07:22 +1200

    Update auto_mount.sh

--- auto_mount.sh
+++ auto_mount.sh
@@ -19,3 +19,11 @@ sudo mkdir /opt/boomi/local
 sudo chmod -R 777 /opt/boomi/
 sudo chmod -R 777 /opt/boomi/local
 sudo chmod -R 777 /tmp
+
+sudo cp /data/jdk-8u211-linux-x64.tar.gz .
+sudo mkdir /opt/java-jdk
+sudo tar -C /opt/java-jdk -zxf ~/jdk-8u211-linux-x64.tar.gz
+sudo update-alternatives --install /usr/bin/java java /opt/java-jdk/jdk1.8.0_211/bin/java 1
+sudo update-alternatives --install /usr/bin/javac javac /opt/java-jdk/jdk1.8.0_211/bin/javac 1
+
+export INSTALL4J_JAVA_HOME=/opt/sslexplorer/jre
